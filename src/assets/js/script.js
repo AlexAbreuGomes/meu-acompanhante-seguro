18b0ec75eaef91cacd680253d068f632832c8ccd
AlexAbreu <alex.abreu.softwaredeveloper@gmail.com>
2024-06-05 00:34:28 -0300

    att dos detalhes e esboço modo dark

---
+++ src/assets/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    console.log('Modo escuro ativado ou desativado');
+}
