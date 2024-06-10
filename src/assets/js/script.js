24220a8ae9b35514be9e09589c14f1a72b09c01c
AlexAbreu <alex.abreu.softwaredeveloper@gmail.com>
2024-06-10 00:41:21 -0300

    dark mode finalizado

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,13 @@
-// script.js
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-}
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-  
-}
+const modoEscuro = document.getElementById('modoEscuro');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+
+
+modoEscuro.addEventListener('click', ()=>{
+    modoEscuro.classList.toggle('dark')
+    body.classList.toggle('dark')
+    
+    console.log('botao clicado')
+});
+
+
