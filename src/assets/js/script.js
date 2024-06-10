e20faece21ebb781fed3d649815ab8efd67c46a6
Alex de Abreu <alex.abreu.softwaredeveloper@gmail.com>
2024-06-10 11:39:56 -0300

    faltando medias querys, fonte e leitura

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,12 +1,9 @@
 const modoEscuro = document.getElementById('modoEscuro');
 const body = document.querySelector('body');
-const header = document.querySelector('header');
-
 
 modoEscuro.addEventListener('click', ()=>{
     modoEscuro.classList.toggle('dark')
     body.classList.toggle('dark')
-    
     console.log('botao clicado')
 });
 
